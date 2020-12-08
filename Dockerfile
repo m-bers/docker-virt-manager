edbd3eaf52b6f4e8cefa2a7225cdfcadcadd02c0
Josh Chambers <jc4gavejc@gmail.com>
2020-12-07 21:23:17 -0800

    run as root

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,14 @@
 FROM ubuntu
 
+ENV GDK_BACKEND=broadway
+ENV BROADWAY_DISPLAY=:5
+
 RUN apt-get update
 RUN apt-get install -y --no-install-recommends libgtk-3-0 libgtk-3-bin nginx gettext-base virt-manager dbus-x11 libglib2.0-bin gir1.2-spiceclientgtk-3.0 ssh 
 RUN apt-get clean && apt-get autoclean && rm -rf /var/lib/apt/lists/*
 
-RUN useradd -m user
-RUN mkdir -p /home/user/.ssh
-RUN chown -R user:user /home/user/.ssh
-RUN echo "Host *\n\tStrictHostKeyChecking no\n" >> /home/user/.ssh/config
+RUN mkdir -p /root/.ssh
+RUN echo "Host *\n\tStrictHostKeyChecking no\n" >> /root/.ssh/config
 
 COPY start.sh /usr/local/bin/start
 COPY nginx.conf /etc/nginx/nginx.conf
--- start.sh
+++ start.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 set -e
 nginx -g 'daemon off;' &
-su user --command "export GDK_BACKEND=broadway"
-su user --command "export BROADWAY_DISPLAY=:5"
-su user --command "nohup /usr/bin/broadwayd :5 &> /var/log/broadway.log &"
-su user --command 'dbus-launch gsettings set org.virt-manager.virt-manager.connections uris "$AUTOCONNECT"'
-su user --command 'dbus-launch gsettings set org.virt-manager.virt-manager.connections autoconnect "$AUTOCONNECT"'
-su user --command 'virt-manager --no-fork'
+nohup /usr/bin/broadwayd :5 &> /var/log/broadway.log &
+dbus-launch gsettings set org.virt-manager.virt-manager.connections uris "$AUTOCONNECT"
+dbus-launch gsettings set org.virt-manager.virt-manager.connections autoconnect "$AUTOCONNECT"
+virt-manager --no-fork
